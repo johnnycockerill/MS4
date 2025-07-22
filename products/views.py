034950d8cd0e2e91286f81162237b338cc5875f1
johnnycockerill <167190345+johnnycockerill@users.noreply.github.com>
2025-07-22 18:55:42 +0000

    started functionality to enable sorting from dropdown selector - not working

--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Products, Category
+from django.db.models.functions import Lower
 
-# Create your views here.
+from .models import Products, Category
 
 def all_products(request):
     """ A view to show all products, sorting and search """
@@ -21,6 +21,9 @@ def all_products(request):
             if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
+
+            if sortkey == 'category':
+                sortkey = 'category__name'
 
             if 'direction' in request.GET:
                 direction = request.GET['direction']
